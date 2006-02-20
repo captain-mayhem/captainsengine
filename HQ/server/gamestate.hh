82f02dc62c43afb0d635629362d59c300c7a691a
marco <marco@dc34a4ac-3e42-4537-918b-2dea8b01751e>
2006-02-20 11:14:04 +0000

    importing sources
    git-svn-id: svn://svn.code.sf.net/p/captainsengine/code/trunk@5 dc34a4ac-3e42-4537-918b-2dea8b01751e

---
+++ HQ/server/gamestate.hh
@@ -0,0 +1,106 @@
+/***************************************************************************
+                          gamestate.hh -  description
+                             -------------------
+    begin                : Di Mär 22 2005
+    copyright            : (C) 2005 by Marco Staginski
+    email                : captain@captain-online.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef GAMESTATE_HH
+#define GAMESTATE_HH
+
+#include "serversocket.hh"
+#include "../heroquest/vector.hh"
+
+/**
+  *@author Marco Staginski
+  */
+
+//! the state in which the game is
+enum state{
+  INIT,
+  PREPARE,
+  RUN
+};
+
+//! manages the state of the game
+class GameState {
+public: 
+  //! Constructor
+  GameState();
+  //! Destructor
+  ~GameState();
+  //! set the state of the game
+  inline void setState(enum state status){this->status = status;}
+  //! return the game state
+  inline state getState() const {return status;}
+  //! get the index of the player whose turn it is at the moment
+  inline short getActivePlayerIdx() const {return activePlayer;}
+  //! init the game
+  void init();
+  //! start the game
+  void start();
+  //! get the next player, after the last ended his turn
+  ServerSocket* getNextPlayer();
+  //! returns, if it is this socket that belongs to the currently active player
+  bool isActivePlayer(const ServerSocket* ss) const;
+  //! roll the dice
+  /* \param num how many dice to roll
+   * \param movementDice if these are normal or attack/defend dices
+   */
+  string die(const short num, const bool movementDice, short* s=NULL, short* bs=NULL, short* ws=NULL, short* m=NULL);
+  //! if the player has already moved
+  inline const bool hasMoved(){return moved;}
+  //! if an action was performed, it must be set here
+  void performAction(bool action=true);
+  //! if an action has been performed
+  inline bool isActionPerformed() {return actionPerformed;}
+  //! set the place of the action
+  inline void setActionPlace(Vector2D& pos) {actionPlace = pos;}
+  //! get the place of the action
+  inline Vector2D getActionPlace() {return actionPlace;}
+  //! set the pending damage
+  inline void setPendingDamage(short damage){pendingDamage = damage;}
+  //! get the pending damage
+  inline short getPendingDamage() {return pendingDamage;}
+  //! end the game
+  void end();
+  //! set movement
+  inline void setMovement(short num) {moved = true;moves = num;}
+  //! get movement
+  inline short getMovement() {return moves;}
+  //! end a turn
+  void endTurn(ServerSocket* ss);
+private:
+  //! the gamestate
+  enum state status;
+  //! the currently active player
+  short activePlayer;
+  //! the number of players in the game
+  short numPlayers;
+  //! the socket of the active player
+  ServerSocket* active;
+  //! already moved
+  bool moved;
+  //! an action was already performed
+  bool actionPerformed;
+  //! where the last action took place
+  Vector2D actionPlace;
+  //! the damage that is pending
+  short pendingDamage;
+  //! the currently remaining moves
+  short moves;
+};
+
+extern GameState game;
+
+#endif
