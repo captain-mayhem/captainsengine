b41e1c91e6dcc7350063079fe315b46d7c260e7b
marco <marco@dc34a4ac-3e42-4537-918b-2dea8b01751e>
2006-02-27 18:41:57 +0000

    new files
    git-svn-id: svn://svn.code.sf.net/p/captainsengine/code/trunk@17 dc34a4ac-3e42-4537-918b-2dea8b01751e

---
+++ HQ/editor/make.bat
@@ -0,0 +1,6 @@
+editor maze.map
+editor trial.map
+copy *.hqm ..\heroquest\levels\basic\
+copy *.lua ..\heroquest\levels\basic\
+copy *.hqm ..\server\levels\basic\
+copy *.lua ..\server\levels\basic\
