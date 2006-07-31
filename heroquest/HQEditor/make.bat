e31dba94305e01e9b60e2294fc95958205165f65
marco <marco@dc34a4ac-3e42-4537-918b-2dea8b01751e>
2006-07-31 18:08:35 +0000

    Changes for Windows
    git-svn-id: svn://svn.code.sf.net/p/captainsengine/code/trunk@101 dc34a4ac-3e42-4537-918b-2dea8b01751e

--- heroquest/HQEditor/make.bat
+++ heroquest/HQEditor/make.bat
@@ -1,5 +1,18 @@
 HQEditor basic\maze.map
 HQEditor basic\trial.map
+HQEditor basic\rescueSirRagnar.map
+HQEditor basic\lairOrcWarlord.map
+HQEditor basic\princeMagnusGold.map
+HQEditor basic\melarsMaze.map
+HQEditor basic\LegacyOrcWarlord.map
+HQEditor basic\lostWizard.map
+HQEditor basic\fireMage.map
+HQEditor basic\raceAgainstTime.map
+HQEditor basic\castleMystery.map
+HQEditor basic\bastionChaos.map
+HQEditor basic\barakTor.map
+HQEditor basic\spiritBlade.map
+HQEditor basic\returnBarakTor.map
 copy basic\*.hqm ..\HQClient\levels\basic\
 copy basic\*.lua ..\HQClient\levels\basic\
 copy basic\*.hqm ..\HQServer\levels\basic\
