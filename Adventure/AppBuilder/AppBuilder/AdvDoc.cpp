3dd0545989008ca66154bc341d74ad75ddc852c4
captain_marco <captain_marco@dc34a4ac-3e42-4537-918b-2dea8b01751e>
2009-08-04 20:29:58 +0000

    Continued parsing.
    git-svn-id: svn://svn.code.sf.net/p/captainsengine/code/trunk@252 dc34a4ac-3e42-4537-918b-2dea8b01751e

--- Adventure/AppBuilder/AppBuilder/AdvDoc.cpp
+++ Adventure/AppBuilder/AppBuilder/AdvDoc.cpp
@@ -7,6 +7,8 @@
 
 const int STATES_MAX = 10;
 const int FRAMES_MAX = 25;
+const int FRAMES2_MAX = 30;
+const int PARTS_MAX = 2;
 
 IMPLEMENT_DYNAMIC_CLASS(AdvDocument, wxDocument);
 
@@ -28,15 +30,21 @@ wxInputStream& AdvDocument::LoadObject(wxInputStream& stream){
   wxZipInputStream zipstream(stream);
   wxZipEntry* entry = zipstream.GetNextEntry();
   if (!loadFile1(zipstream)){
+    delete entry;
+    zipstream.CloseEntry();
     wxMessageBox("Failed loading project file", "Error");
     return stream;
   }
   delete entry;
+  zipstream.CloseEntry();
   entry = zipstream.GetNextEntry();
   if(!loadFile2(zipstream)){
+    delete entry;
+    zipstream.CloseEntry();
     wxMessageBox("Failed loading project file", "Error");
     return stream;
   }
+  delete entry;
   zipstream.CloseEntry();
   wxMessageBox("Project loaded successfully", "Info");
   return stream;
@@ -147,7 +155,80 @@ bool AdvDocument::loadFile2(wxInputStream& stream){
     int splitidx = rest.Find(" ");
     wxString type = rest.SubString(0, splitidx-1);
     wxString name = rest.SubString(splitidx+1, rest.Length());
-    break;
+    // ITEM
+    if (type == "Item"){
+      Item it;
+      it.name = name;
+      for (int state = 0; state < STATES_MAX; ++state){
+        Frames frm;
+        for (int frames = 0; frames < FRAMES_MAX; ++frames){
+          str = txtstream.ReadLine();
+          if (str != ";" && str.Length() > 0){
+            frm.push_back(std::string(str.SubString(0,str.Length()-2).c_str()));
+          }
+        }
+        str = txtstream.ReadLine();
+        assert(str == "20");
+        it.frames.push_back(frm);
+      }
+      mItems.push_back(it);
+    }
+    // OBJECT
+    else if (type == "Object"){
+      Object obj;
+      obj.name = name;
+      long val1, val2;
+      str = txtstream.ReadLine();
+      str.ToLong(&val1);
+      str = txtstream.ReadLine();
+      str.ToLong(&val2);
+      obj.size = std::make_pair(val1, val2);
+      str = txtstream.ReadLine();
+      str.ToLong(&val1);
+      obj.unknown = val1;
+      assert(obj.unknown == 0);
+      for (int state = 0; state < STATES_MAX; ++state){
+        ExtendedFrames frms;
+        for (int frames = 0; frames < FRAMES2_MAX; ++frames){
+          ExtendedFrame frm;
+          bool set = false;
+          for (int parts = 0; parts < PARTS_MAX; ++parts){
+            str = txtstream.ReadLine();
+            if (str.Length() > 0){
+              set = true;
+              frm.names.push_back(std::string(str.c_str()));
+            }
+          }
+          //read offsets
+          str = txtstream.ReadLine();
+          for (unsigned i = 0; i < frm.names.size(); ++i){
+            int pos = str.Find(";");
+            wxString num = str.SubString(0, pos-1);
+            str = str.SubString(pos+1, str.Length());
+            num.ToLong(&val1);
+            pos = str.Find(";");
+            num = str.SubString(0, pos-1);
+            str = str.SubString(pos+1, str.Length());
+            num.ToLong(&val2);
+            frm.offsets.push_back(std::make_pair(val1,val2));
+          }
+          if (set)
+            frms.push_back(frm);
+        }
+        str = txtstream.ReadLine();
+        assert(str == "20");
+        obj.frames.push_back(frms);
+      }
+      mObjects.push_back(obj);
+    }
+    // CHARACTER
+    else if (type == "Character"){
+
+    }
+    else{
+      wxMessageBox(type, "Unknown type found");
+      return false;
+    }
   }
   return true;
 }
--- Adventure/AppBuilder/AppBuilder/AdvDoc.h
+++ Adventure/AppBuilder/AppBuilder/AdvDoc.h
@@ -9,10 +9,49 @@
 #include "AdvMainTree.h"
 //#include "Graph.h"
 
+typedef std::vector<std::string> Frames;
+
 struct CursorState{
-  std::vector<std::string> frames;
+  Frames frames;
   int command;
   std::pair<int,int> highlight;
+};
+
+typedef std::vector<CursorState> Cursor;
+
+struct Item{
+  std::string name;
+  std::vector<Frames> frames;
+};
+
+struct ExtendedFrame{
+  std::vector<std::string> names;
+  std::vector<std::pair<int,int> > offsets;
+};
+
+typedef std::vector<ExtendedFrame> ExtendedFrames;
+
+struct Object{
+  std::string name;
+  std::pair<int,int> size;
+  int unknown;
+  std::vector<ExtendedFrames> frames;
+};
+
+struct Character{
+  std::string name;
+  int textcolor;
+  int walkspeed;
+  bool notzoom;
+  bool realleft;
+  bool memresistent;
+  bool ghost;
+  std::string walksound;
+  int unknown;
+  int zoom;
+  std::pair<int,int> size;
+  std::pair<int,int> basepoint;
+  std::vector<ExtendedFrames> frames;
 };
 
 class AdvDocument : public wxDocument{
@@ -30,7 +69,9 @@ protected:
   static int insertTreeElement(wxTreeCtrl* tree, const wxString& name, wxTreeItemId* current, int curr_level);
   AdvMainTreeView* mView;
   std::map<wxString,wxFileName> mImageNames;
-  std::vector<CursorState> mCursor;
+  Cursor mCursor;
+  std::vector<Item> mItems;
+  std::vector<Object> mObjects;
   //Graph mGamepool;
 };
 
