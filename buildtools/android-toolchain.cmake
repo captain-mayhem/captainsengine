ea807a767b4269063519542e54387ecac0e6c2c7
captain_marco <captain_marco@dc34a4ac-3e42-4537-918b-2dea8b01751e>
2012-04-30 18:45:25 +0000

    updated Android toolchain to work also under Linux
    git-svn-id: svn://svn.code.sf.net/p/captainsengine/code/trunk@624 dc34a4ac-3e42-4537-918b-2dea8b01751e

--- buildtools/android-toolchain.cmake
+++ buildtools/android-toolchain.cmake
@@ -5,10 +5,15 @@ SET(CMAKE_SYSTEM_NAME Linux)
 SET(PLATFORM_NDK_PATH "$ENV{ANDROID_NDK_ROOT}/platforms/android-8")
 SET(PLATFORM_SDK_PATH "$ENV{ANDROID_SDK_ROOT}/platforms/android-8")
 
+IF (CMAKE_HOST_UNIX)
+SET(PLATFORM_NAME linux-x86)
+ELSE()
 SET(PLATFORM_NAME windows)
+SET(EXE_SUFFIX .exe)
+ENDIF()
 
-CMAKE_FORCE_C_COMPILER("$ENV{ANDROID_NDK_ROOT}/toolchains/arm-linux-androideabi-4.4.3/prebuilt/${PLATFORM_NAME}/bin/arm-linux-androideabi-gcc.exe" GNU)
-CMAKE_FORCE_CXX_COMPILER("$ENV{ANDROID_NDK_ROOT}/toolchains/arm-linux-androideabi-4.4.3/prebuilt/${PLATFORM_NAME}/bin/arm-linux-androideabi-g++.exe" GNU)
+CMAKE_FORCE_C_COMPILER("$ENV{ANDROID_NDK_ROOT}/toolchains/arm-linux-androideabi-4.4.3/prebuilt/${PLATFORM_NAME}/bin/arm-linux-androideabi-gcc${EXE_SUFFIX}" GNU)
+CMAKE_FORCE_CXX_COMPILER("$ENV{ANDROID_NDK_ROOT}/toolchains/arm-linux-androideabi-4.4.3/prebuilt/${PLATFORM_NAME}/bin/arm-linux-androideabi-g++${EXE_SUFFIX}" GNU)
 
 SET(CMAKE_FIND_ROOT_PATH "$ENV{ANDROID_NDK_ROOT}/toolchains/arm-linux-androideabi-4.4.3/${PLATFORM_NAME}")
 
@@ -16,8 +21,13 @@ SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-INCLUDE_DIRECTORIES(${PLATFORM_NDK_PATH}/arch-arm/usr/include)
+INCLUDE_DIRECTORIES(
+${PLATFORM_NDK_PATH}/arch-arm/usr/include
+$ENV{ANDROID_NDK_ROOT}/sources/cxx-stl/gnu-libstdc++/include
+$ENV{ANDROID_NDK_ROOT}/sources/cxx-stl/gnu-libstdc++/libs/armeabi/include
+)
 
+SET(LINK_DIR ${PLATFORM_NDK_PATH}/arch-arm/usr/lib) 
 SET(CMAKE_SHARED_LINKER_FLAGS
--Wl,--no-undefined
+"-Wl,--no-undefined --sysroot=${PLATFORM_NDK_PATH}/arch-arm -L${LINK_DIR} -L$ENV{ANDROID_NDK_ROOT}/sources/cxx-stl/gnu-libstdc++/libs/armeabi -lgnustl_shared"
 )
