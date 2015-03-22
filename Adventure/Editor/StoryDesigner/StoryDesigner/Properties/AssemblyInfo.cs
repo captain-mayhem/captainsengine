deb2e6ec310f70ba74c8066ebdb6668d7360c8a9
captain_marco <captain_marco@dc34a4ac-3e42-4537-918b-2dea8b01751e>
2015-03-22 11:27:11 +0000

    Generation of engine.dat Generation of update.xml
    git-svn-id: svn://svn.code.sf.net/p/captainsengine/code/trunk@1008 dc34a4ac-3e42-4537-918b-2dea8b01751e

--- Adventure/Editor/StoryDesigner/StoryDesigner/Properties/AssemblyInfo.cs
+++ Adventure/Editor/StoryDesigner/StoryDesigner/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("www.captainsengine.sourceforge.net")]
 [assembly: AssemblyProduct("StoryDesigner")]
-[assembly: AssemblyCopyright("Copyright © 2014 Marco Staginski")]
+[assembly: AssemblyCopyright("Copyright © 2015 Marco Staginski")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
