2a8d13e15fe358119a84ff090ceb2b7acc8568ad
captain <captain@obfuscated.de>
2015-07-22 19:23:58 +0200

    Removed old tags and branches from subversion

--- tags/heroquest_0.7/Engine/utilities/obj2hmb/make.bat
+++
@@ -1,7 +0,0 @@
-obj2hmb -k ../../../heroquest/HQClient/models/world/ground.obj
-move ..\..\..\heroquest\HQClient\models\world\ground.hmb ..\..\..\heroquest\HQClient\models\
-obj2hmb -k ../../../heroquest/HQClient/models/world/pit2.obj
-move ..\..\..\heroquest\HQClient\models\world\pit2.hmb ..\..\..\heroquest\HQClient\models\pit.hmb
-obj2hmb -f ../../../heroquest/HQClient/models/world/rocktrap.obj
-move ..\..\..\heroquest\HQClient\models\world\rocktrap.hmb ..\..\..\heroquest\HQClient\models\
-@set /P %path=Done
